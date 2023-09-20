820d5fd3763ab226552325e7588ebaac3c1af643
DanielaCF3 <vicaimerhil@gmail.com>
2023-09-20 03:24:25 -0300

    Bucles

---
+++ bucles.js
@@ -0,0 +1,22 @@
+for(var i=1; i<20; i+=2){
+    if(i<20){
+        console.log(i);
+    }
+}
+for(var ii=3; ii<100; ii+=3){
+    if(ii<100){
+        console.log(ii);
+    }
+    
+}
+let suma=0;
+for(var i=1; i<101; i++){
+    suma +=i;
+}
+console.log(suma);
+
+let product = 1;
+for(var i=1; i<13; i++){
+    product *= i;
+}
+console.log(product);
